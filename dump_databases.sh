f3918f36f9ac422e9b408a1b1f479a4e11143b6f
Andriy Gushuley <andriy.gushuley@playtech.com>
2016-10-18 14:13:08 +0300

    Dump script fixes.

--- dump_databases.sh
+++ dump_databases.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 
+SCRIPT_NAME="$( basename $0 )"
+if [[ "$( uname )" == "Darwin" ]] ; then
+    BASE_NAME="$( dirname $( greadlink -f $0 ) )"
+else
+    BASE_NAME="$( dirname $( readlink -f $0 ) )"
+fi
+
+function print_error() {
+	echo $( date "+%y-%m-%d %H:%M:%S" ) ${SCRIPT_NAME} $@ 1>&2
+}
+
+function panic() {
+    print_error $@
+    exit 1
+}
+
+function dir_check_scm() { echo "Have to be defined $FUNCNAME $1 $2 $3"; exit 1; }
+function dir_push_scm() { echo "Have to be defined $FUNCNAME $1 $2"; exit 1; }
+source ${BASE_NAME}/scm.sh \
+    || panic "$0 Can't load scm library "
+
 cd /tmp
 
 PG_BACKUPS=${PG_BACKUPS:-N}
-PG_BACKUPS_DIR="${PG_BACKUPS_DIR:-~/var/backups/pg}"
+PG_BACKUPS_DIR="${PG_BACKUPS_DIR:-$HOME/var/backups/pg}"
 # if databases are empty, all databases will be backed up, except ( "template*" )
 PG_DATABASES="${PG_DATABASES:-}"
 # if no, global objects will not be dumped (pg_dumpall -g)
@@ -13,28 +34,19 @@ PG_NO_GLOBALS="${PG_NO_GLOBALS:-}"
 # mode=host, psql -H ${pg_backups_host}  will be used, user param -P will be added
 # if they are not empty, if password is not empty, PGPASSWORD variable will be set, or
 # password can be written into ~/.pgpass file
-PG_CONNECTION=
-PG_HOST=
-PG_USER=
-PG_PASSWORD=
+MYSQL_BACKUPS=${MYSQL_BACKUPS:-}
+PG_HOST=${PG_HOST:-}
+PG_USER=${PG_USER:-}
+PG_PASSWORD=${PG_PASSWORD:-}
 
-MYSQL_ENABLED=
-MYSQL_BACKUPS_DIR="${MYSQL_BACKUPS_DIR:-~/var/backups/mysql}"
+MYSQL_BACKUPS=${MYSQL_BACKUPS:-N}
+MYSQL_BACKUPS_DIR="${MYSQL_BACKUPS_DIR:-$HOME/var/backups/mysql}"
 # if databases are empty, all databases will be backed up
-MYSQL_DATABASES=
-# plain mysqldump call will be used with host, user, pass added, if wariables will be difined
-MYSQL_HOST=
-MYSQL_USER=
-MYSQL_PASSWORD=
-
-function dir_check_scm() { echo "Have to be defined $FUNCNAME $1 $2 $3"; exit 1; }
-function dir_push_scm() { echo "Have to be defined $FUNCNAME $1 $2"; exit 1; }
-{ scm_sh=$( dirname $0 )/scm.sh &&
-source ${scm_sh}
-} || {
-    echo "$0 Can't load scm library " 1>&2
-    exit 1
-}
+MYSQL_DATABASES=${MYSQL_DATABASES:-}
+# plain mysqldump call will be used with host, user, pass added, if variables will be difined
+MYSQL_HOST=${MYSQL_HOST:-}
+MYSQL_USER=${MYSQL_USER:-}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-}
 
 case "${PG_BACKUPS}" in
     [Yy][Ee][Ss]|[Yy]|[1]|[Tt][Rr][Uu][Ee])
@@ -67,8 +79,7 @@ case "${PG_BACKUPS}" in
                 pg_suffix=""
             ;;
             *)
-                echo "$0 invalid PG_CONNECTION value ${PG_CONNECTION}, has to be sudo|host or empty" 1>&2
-                exit 1
+                panic "Invalid PG_CONNECTION value ${PG_CONNECTION}, has to be sudo|host or empty"
             ;;
         esac
 
@@ -76,48 +87,48 @@ case "${PG_BACKUPS}" in
             PG_DATABASES=$( ${pg_prefix} psql ${pg_suffix} -t -c "select datname from pg_database where datname not like 'template%';" postgres )
         fi
 
-        mkdir -p "${PG_BACKUPS_DIR}" || {
-        echo "$0 Can't create target databases dump dir ${PG_BACKUPS_DIR}" 1>&2
-        exit 1
-    }
+        mkdir -p "${PG_BACKUPS_DIR}" || panic "Can't create target databases dump dir ${PG_BACKUPS_DIR}"
 
         case "${PG_NO_GLOBALS}" in
             [Yy][Ee][Ss]|[Yy1]|[Tt][Rr][Uu][Ee])
             ;;
             *)
+
                 LOCKFILE=${PG_BACKUPS_DIR}/globals.LOCK
                 if ( set -o noclobber; echo "locked" > "${LOCKFILE}") 2> /dev/null ; then
                     trap "rm -f \"${LOCKFILE}\"" INT TERM EXIT
-                    _scm=
-                    mkdir -p "${PG_BACKUPS_DIR}/globals" \
-                && dir_check_scm "${PG_BACKUPS_DIR}/globals" _scm "Y" \
-                && ${pg_prefix} pg_dumpall ${pg_suffix} -g > ${PG_BACKUPS_DIR}/globals/globals.sql \
-                && dir_push_scm "${PG_BACKUPS_DIR}/globals" "${_scm}" \
-            || echo "$0 Error backup database server globals" 1>&2
+                    {
+                        _scm=
+                        mkdir -p "${PG_BACKUPS_DIR}/globals" \
+                            && dir_check_scm "${PG_BACKUPS_DIR}/globals" _scm "Y" \
+                            && ${pg_prefix} pg_dumpall ${pg_suffix} -g > ${PG_BACKUPS_DIR}/globals/globals.sql \
+                            && dir_push_scm "${PG_BACKUPS_DIR}/globals" "${_scm}"
+                    } || print_error "Error backup database server globals"
                     rm -f ${LOCKFILE}
                 else
-                    echo "$0 pgglobals lock file ${LOCKFILE} exist, globals backup skipped" 1>&2
+                    print_error "pgglobals lock file ${LOCKFILE} exist, globals backup skipped"
                 fi
         esac
 
-        pg_split=$( dirname $0 )/pg_dump_splitsort.py
+        pg_split="${BASE_NAME}/pg_dump_splitsort.py"
 
         for database in ${PG_DATABASES} ; do
             LOCKFILE=${PG_BACKUPS_DIR}/${database}.LOCK
             if ( set -o noclobber; echo "locked" > "${LOCKFILE}") 2> /dev/null ; then
                 trap "rm -f \"${LOCKFILE}\"" INT TERM EXIT
             else
-                echo "$0 Database lock file ${LOCKFILE} exist, database ${database} backup skipped" 1>&2
+                print_error "Database lock file ${LOCKFILE} exist, database ${database} backup skipped"
                 continue
             fi
-            _scm=
-            sql_dump_file=${PG_BACKUPS_DIR}/${database}.sql
-            mkdir -p "${PG_BACKUPS_DIR}/${database}" \
-            && ${pg_prefix} pg_dump ${pg_suffix} ${database} > ${sql_dump_file} \
-            && dir_check_scm "${PG_BACKUPS_DIR}/${database}" _scm "Y" \
-            && ${pg_split} ${sql_dump_file} -d ${PG_BACKUPS_DIR}/${database}/ -c \
-            && dir_push_scm "${PG_BACKUPS_DIR}/${database}" "${_scm}" \
-        || echo "$0 Error backuping database ${database}" 1>&2
+            {
+                _scm=
+                sql_dump_file=${PG_BACKUPS_DIR}/${database}.sql
+                mkdir -p "${PG_BACKUPS_DIR}/${database}" \
+                    && ${pg_prefix} pg_dump ${pg_suffix} ${database} > ${sql_dump_file} \
+                    && dir_check_scm "${PG_BACKUPS_DIR}/${database}" _scm "Y" \
+                    && "${pg_split}" ${sql_dump_file} -d ${PG_BACKUPS_DIR}/${database}/ -c \
+                    && dir_push_scm "${PG_BACKUPS_DIR}/${database}" "${_scm}"
+            } || print_error "$0 Error backuping database ${database}"
             rm -f ${sql_dump_file} ${LOCKFILE}
         done
     ;;
@@ -125,13 +136,12 @@ case "${PG_BACKUPS}" in
     ;;
 esac
 
-case "${MYSQL_ENABLED}" in
+case "${MYSQL_BACKUPS}" in
     [Yy][Ee][Ss]|[Yy]|[1]|[Tt][Rr][Uu][Ee])
-        mysql_prefix=""
         mysql_suffix=""
-
+        mysql_env=""
         if [[ ! -z "${MYSQL_PASSWORD}" ]] ; then
-            mysql_suffix="${mysql_suffix} --password=${MYSQL_PASSWORD}"
+            mysql_env="MYSQL_PWD=\"${MYSQL_PASSWORD}\" ${mysql_env}"
         fi
 
         if [[ ! -z "${MYSQL_USER}" ]] ; then
@@ -139,41 +149,44 @@ case "${MYSQL_ENABLED}" in
         fi
 
         if [[ ! -z "${MYSQL_HOST}" ]] ; then
-            mysql_suffix="${mysql_suffix} --host=${PG_HOST}"
+            mysql_suffix="${mysql_suffix} --host=${MYSQL_HOST}"
+        fi
+
+        if [[ ! -z "${MYSQL_PORT}" ]] ; then
+            mysql_suffix="${mysql_suffix} --port=${MYSQL_PORT}"
         fi
 
         if [[ -z "${MYSQL_DATABASES}" ]] ; then
-            MYSQL_DATABASES=$( echo 'show databases' | mysql ${mysql_suffix} -N )
+            MYSQL_DATABASES=$( echo 'show databases' | eval ${mysql_env} mysql ${mysql_suffix} -N )
         fi
 
-        mkdir -p "${MYSQL_BACKUPS_DIR}" || {
-        echo "$0 Can't create target databases dump dir ${MYSQL_BACKUPS_DIR}" 1>&2
-        exit 1
-    }
+        mkdir -p "${MYSQL_BACKUPS_DIR}" \
+            || panic "Can't create target databases dump dir ${MYSQL_BACKUPS_DIR}"
 
-        mysql_split="$( dirname $0 )/mysqldump_splitsort.py"
+        mysql_split="${BASE_NAME}/mysqldump_splitsort.py"
 
         for database in ${MYSQL_DATABASES} ; do
             if [[ ( "${database}" == "information_schema" ) || \
-            ( "${database}" == "performance_schema" ) ]] ; \
-        then
+                ( "${database}" == "performance_schema" ) ]] ; \
+            then
                 continue
             fi
             LOCKFILE=${MYSQL_BACKUPS_DIR}/${database}.LOCK
             if ( set -o noclobber; echo "locked" > "${LOCKFILE}") 2> /dev/null ; then
                 trap "rm -f \"${LOCKFILE}\"" INT TERM EXIT
             else
-                echo "$0 Database lock file ${LOCKFILE} exist, database ${database} backup skipped" 1>&2
+                print_error"Database lock file ${LOCKFILE} exist, database ${database} backup skipped"
                 continue
             fi
-            _scm=
-            sql_dump_file=${MYSQL_BACKUPS_DIR}/${database}.sql
-            mkdir -p "${MYSQL_BACKUPS_DIR}/${database}" \
-            && ${mysql_prefix} mysqldump ${mysql_suffix} --result-file=${sql_dump_file} ${database} -c --skip-opt --skip-dump-date \
-            && dir_check_scm "${MYSQL_BACKUPS_DIR}/${database}" _scm "Y" \
-            && ${mysql_split} ${sql_dump_file} -d ${MYSQL_BACKUPS_DIR}/${database}/ -c \
-            && dir_push_scm "${MYSQL_BACKUPS_DIR}/${database}" "${_scm}" \
-        || echo "$0 Error backuping database ${database}" 1>&2
+            {
+                _scm=
+                sql_dump_file=${MYSQL_BACKUPS_DIR}/${database}.sql
+                mkdir -p "${MYSQL_BACKUPS_DIR}/${database}" \
+                    && eval ${mysql_env} mysqldump ${mysql_suffix} --result-file=${sql_dump_file} ${database} -c --skip-opt --skip-dump-date \
+                    && dir_check_scm "${MYSQL_BACKUPS_DIR}/${database}" _scm "Y" \
+                    && "${mysql_split}" ${sql_dump_file} -d ${MYSQL_BACKUPS_DIR}/${database}/ -c \
+                    && dir_push_scm "${MYSQL_BACKUPS_DIR}/${database}" "${_scm}"
+            } || print_error "Error backuping database ${database}" 1>&2
             rm -f ${sql_dump_file} ${LOCKFILE}
         done
     ;;
